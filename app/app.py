6948880b306921d83c43011cef80f0cae1b2138a
Anviti <srivastavanviti@gmail.com>
2020-08-29 20:05:47 +0530

    Scheduler added-To mark tickets expired

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import (Flask, render_template, request,Blueprint)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-#Initializing Database
+from apscheduler.schedulers.background import BackgroundScheduler
+
 db = SQLAlchemy()
+scheduler = BackgroundScheduler(daemon=True)    
 
 
 def create_app():
@@ -11,14 +13,31 @@ def create_app():
     app.config.from_object('config.Config')
     db.init_app(app)
 
-    from .models import Ticket,User
-    
-    with app.app_context():
+    #Importing Databse Models
+    from .models import Ticket
 
-        #Register BluePrint
+    with app.app_context():
+        #Desiging the scheduler function
+        from .utilites import isActive 
+        def markExpiredTickets():
+            with app.app_context():
+                ticket_list = Ticket.query.all()
+                for ticket in ticket_list:
+                    if not ticket.isExpired and not isActive(ticket.showTime):
+                        result = Ticket.query.filter_by(ticketId = ticket.ticketId).update(dict(isExpired = True))
+                        db.session.commit()
+            return
+
+
+        #Registering Blueprints
         from .ticket_api import ticket_bp as ticket_blueprint
         app.register_blueprint(ticket_blueprint)
         
+        #Initializing Scheduler - marking of tickets as expired will occur every 5 seconds
+        scheduler.add_job(markExpiredTickets, 'interval', seconds=5)          
+        scheduler.start()
+        
+
         #db.create_all()
         return app
     
--- app/utilites.py
+++ app/utilites.py
@@ -2,13 +2,15 @@ from .app import db
 from .models import Ticket,User
 from datetime import datetime, timedelta
 
+"""
+	Function to find the difference current time and given time
+"""
 def isActive(time):
 	#Converting strings to dates
 	datetimeFormat = '%Y-%m-%d %H:%M:%S.%f'
 	date = datetime.strptime(time, datetimeFormat)
 	dif = datetime.now()-date
-	#print(dif.seconds)
 	ans = divmod(dif.total_seconds(), 60)
-	if ans[0]<480:
+	if ans[0]<480 :		#480 minutes = 8hours
 		return True
 	return False
