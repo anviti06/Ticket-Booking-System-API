c911ea1be8e7abee0d539738e5a156b844aa847a
Anviti <srivastavanviti@gmail.com>
2020-08-29 13:14:08 +0530

    Basic app & database added

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from flask import (Flask, render_template, request,Blueprint)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-#from apscheduler.schedulers.background import BackgroundScheduler
 
 db = SQLAlchemy()
-#scheduler = BackgroundScheduler(daemon=True)    
 
 
 def create_app():
@@ -14,31 +12,11 @@ def create_app():
     db.init_app(app)
 
     from .models import Ticket,User
-    print(datetime.now())
-
+    
     with app.app_context():
-        """def check_price():
-            with app.app_context():
-                waitlist_table = Waitlist.query.all()
-        
-                for row in waitlist_table:
-                    temp_product = Product.query.filter_by(pid = row.pid).first()
-                    #print(temp_product.name)
-                    if(temp_product.price <= row.threshold):
-                        #print("Found product")
-                        raise_notification(row.id , temp_product.pid)
-                    #else:
-                        #print("Not Found")
-            return
-            """
 
         from .ticket_api import ticket_bp as ticket_blueprint
         app.register_blueprint(ticket_blueprint)
-        
-        
-        #Initializing Scheduler - event will occur every 5 seconds
-        #scheduler.add_job(check_price, 'interval', seconds=5)          
-        #scheduler.start()
         
         #db.create_all()
         return app
